960adfc36c32d3d98221a4135822a8ea4f5b0f35
Chris Coyier <chriscoyier@gmail.com>
2016-09-12 09:33:44 -0500

    adding test

---
+++ tests/apple-exporter/components/test-class-pre.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/class-component-testcase.php';
+
+use Apple_Exporter\Components\Pre as Pre;
+
+class Pre_Test extends Component_TestCase {
+
+  public function testBuildingRemovesTags() {
+    $component = new Quote( '<pre><code data-lang="javascript">var x = {
+  "foo": 1;
+}<code></pre>',
+      null, $this->settings, $this->styles, $this->layouts );
+
+    $result_wrapper = $component->to_array();
+    $result = $result_wrapper['components'][0];
+    $this->assertEquals( 'container', $result_wrapper['role'] );
+    $this->assertEquals( 'body', $result['role'] );
+    $this->assertEquals( "var x = {\r\n \"foo\": 1;\r\n}", $result['text'] );
+    $this->assertEquals( 'markdown', $result['format'] );
+    $this->assertEquals( 'default-pre', $result['textStyle'] );
+    $this->assertEquals( 'pre-layout', $result['layout'] );
+  }
+
+}
+
