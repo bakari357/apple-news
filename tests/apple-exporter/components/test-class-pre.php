a1ca890b815c676c658cd3e303837f06f7584a36
bcampeau <brad@alleyinteractive.com>
2016-09-13 20:02:59 -0600

    Fixed unit test for pre support

--- tests/apple-exporter/components/test-class-pre.php
+++ tests/apple-exporter/components/test-class-pre.php
@@ -8,15 +8,18 @@ class Pre_Test extends Component_TestCase {
 
   public function testBuildingRemovesTags() {
     $component = new Pre( '<pre><code data-lang="javascript">var x = {
-  "foo": 1;
+	"foo": 1;
 }</code></pre>',
-      null, $this->settings, $this->styles, $this->layouts );
+	null, $this->settings, $this->styles, $this->layouts
+	);
 
     $result_wrapper = $component->to_array();
     $result = $result_wrapper['components'][0];
     $this->assertEquals( 'container', $result_wrapper['role'] );
     $this->assertEquals( 'body', $result['role'] );
-    $this->assertEquals( "<code data-lang=\"javascript\">var x = {\r\n\"  foo\": 1;\r\n}</code>", $result['text'] );
+    $this->assertEquals( '<code data-lang="javascript">var x = {
+	"foo": 1;
+}</code>', $result['text'] );
     $this->assertEquals( 'markdown', $result['format'] );
     $this->assertEquals( 'default-pre', $result['textStyle'] );
     $this->assertEquals( 'pre-layout', $result['layout'] );
